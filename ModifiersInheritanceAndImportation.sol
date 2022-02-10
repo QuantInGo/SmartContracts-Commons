f15b69e61dcfd9ca7ef03eb8945be6624f8fc0b2
Rego <pvrego@users.noreply.github.com>
2022-02-10 07:19:13 -0300

    Create ModifiersInheritanceAndImportation.sol

---
+++ ModifiersInheritanceAndImportation.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ModifiersInheritanceAndImporting{
+    
+    mapping(address => uint) public tokenBalance;
+    address owner;
+    uint tokenPrice = 1 ether;
+
+    constructor(){
+        owner = msg.sender;
+        tokenBalance[owner]= 100;
+    }
+
+    function createNewToken(){
+        require(msg.sender == owner, 'Not the owner : not allowed to create a token.')
+        tokenBalance[owner]++;
+    }
+
+    function burnToken() public{
+       require(msg.sender == owner, 'Not the owner : not allowed to burn tokens.') 
+       tokenBalance[owner]--;
+    }
+
+    function purchaseToken() public payable{
+        require((tokenBalance[owner]*tokenPrice)/msg.value > 0, 'Not enough tokens.');
+        tokenBalance[owner] -= msg.value/ tokenPrice;
+        tokenBalance[msg.sender] += msg.value / tokenPrice;
+    }
+
+}
