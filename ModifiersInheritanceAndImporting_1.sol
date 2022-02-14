e9cc758a1de7acf75e15fdcf2b706ed4cb697d7a
Rego <pvrego@users.noreply.github.com>
2022-02-14 16:55:39 -0300

    Update ModifiersInheritanceAndImporting_1.sol

--- ModifiersInheritanceAndImporting_1.sol
+++ ModifiersInheritanceAndImporting_1.sol
@@ -2,28 +2,21 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract OwnerPack{
+contract ModifiersInheritanceAndImporting{
+    
+    mapping(address => uint) public tokenBalance;
     address owner;
+    uint tokenPrice = 1 ether;
 
     constructor(){
         owner = msg.sender;
+        tokenBalance[owner]= 100;
     }
 
     modifier onlyOwner(){
         require(msg.sender == owner, 'Not owner : not allowed to execute this action.');
         _;
     }
-}
-
-contract ModifiersInheritanceAndImporting is OwnerPack{
-    
-    mapping(address => uint) public tokenBalance;
-    uint tokenPrice = 1 ether;
-
-    constructor(){
-        tokenBalance[owner]= 100;
-    }
-
 
     function createNewToken() public onlyOwner{
         tokenBalance[owner]++;
